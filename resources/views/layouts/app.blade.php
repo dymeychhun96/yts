68a8358fd26a9b78e5e4e7a3fd8985a2e6ea0935
dymeychhun <dymey@angkorhospital.org>
2026-03-09 08:34:41 +0700

    create 9 files and update 7 files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="{{ asset('storage/images/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('public/images/favicon.ico') }}" type="image/x-icon">
     <title>YTS</title>
 
     <!-- style -->
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,35 +1,8 @@
-<!--<nav class="navbar navbar-expand-lg bg-body-tertiary py-3">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('home') }}">
-            <img src="{{ asset('storage/images/logo-YTS.svg') }}" alt="YTS Logo">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
-                </li>
-                {{-- <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li> --}}
-</ul>
-<form class="d-flex" role="search" action="{{ route('search') }}" method="get">
-    <input class="form-control rounded" type="search" placeholder="Search" aria-label="Search"
-        aria-describedby="search-addon" name="query_term" autocomplete="off" />
-    <span class="input-group-text border-0 material-symbols-rounded fs-4">
-        search
-    </span>
-</form>
-</div>
-</div>
-</nav>-->
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('home') }}">Navbar</a>
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <img src="{{ asset('public/images/logo-YTS.svg') }}" alt="Logo YTS">
+        </a>
         <button data-mdb-collapse-init class="navbar-toggler" type="button" data-mdb-target="#navbarMenu"
             aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="material-symbols-rounded" id="hamburgerIcon">
